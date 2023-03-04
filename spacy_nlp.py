4cfe2558d5eaf2e68d85361454ef675964f9a2ca
param108 <param.ponnaiyan@gmail.com>
2023-03-04 09:14:44 +0530

    Adding en_core_web_md

--- spacy_nlp.py
+++ spacy_nlp.py
@@ -1,1 +1,21 @@
 import spacy
+from spacy.matcher import Matcher
+
+nlp = spacy.load("en_core_web_md")
+
+lemmatizer = nlp.get_pipe("lemmatizer")
+doc = nlp("South of Dracos point there is a small village. The village's name is tintorin")
+
+print([token.lemma_ for token in doc])
+matcher = Matcher(nlp.vocab)
+
+pattern = [{"POS": "NOUN"}]
+
+#matcher.add("ALL_NOUNS", [pattern])
+
+matcher.add("VILLAGE_NAME", [[{"LEMMA": "village"}, {"LEMMA": "name"}]])
+matches = matcher(doc)
+
+for match_id, start, end in matches:
+    matched_span = doc[start:end]
+    print(matched_span.text)
